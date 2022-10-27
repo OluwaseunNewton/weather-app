4639b9f725595d23a17252c256ed2f372250a07b
OluwaseunNewton <113258885+OluwaseunNewton@users.noreply.github.com>
2022-10-27 17:36:47 +0200

    changed the API to SheCodes

--- src/index.js
+++ src/index.js
@@ -25,34 +25,36 @@ let currentDay = document.querySelector("#current-day");
 currentDay.innerHTML = showDay(now);
 
 function showCityTemp(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#city").innerHTML = response.data.city;
   document.querySelector("#current-temp").innerHTML = Math.round(
-    response.data.main.temp
+    response.data.temperature.current
   );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#feel").innerHTML = Math.round(
-    response.data.main.feels_like
+    response.data.temperature.feels_like
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].description;
+    response.data.condition.description;
 
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 function enterCity(event) {
   event.preventDefault();
-  let apiKey = "a867e25f2d83db579421a57fd8e937ec";
+  let apiKey = "39te550do00a1b7f4f7ed027cbfc823a";
+  let apiEndpoint = "https://api.shecodes.io/weather/v1/current?";
   let unit = "metric";
   let city = document.querySelector("#city-input").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  let apiUrl = `${apiEndpoint}query=${city}&key=${apiKey}&units=${unit}`;
 
   axios.get(apiUrl).then(showCityTemp);
 }
